9597ffaa9713ab09eb339651c15d46c7ba95f0f1
Elynor Sykes <esykes@cherrycreekschools.org>
2024-12-13 08:42:22 -0700

    fixed chgPasswd()

--- Linux_Ubuntu.sh
+++ Linux_Ubuntu.sh
@@ -269,25 +269,32 @@ createUser() {
 
 ##Changes all the user passwords
 chgPasswd(){
-echo "$LogTime uss: [$UserName]# Changing all the user passwords to Cyb3rPatr!0t$." >> output.log
-	##Look for valid users that have different UID that not 1000+
-	cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1 > users
-	##Looks for users with the UID and GID of 0
-	hUSER=`cut -d: -f1,3 /etc/passwd | egrep ':[0]{1}$' | cut -d: -f1`
-	echo "$hUSER is a hidden user"
-	sed -i '/root/ d' users
+    LogTime=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "$LogTime uss: [$UserName]# Changing all user passwords except for the current user to Cyb3rPatr!0t$." >> output.log
 
-	PASS='Cyb3rPatr!0t$'
-	for x in `cat users`
-	do
-		echo -e "$PASS\n$PASS" | passwd $x >> output.log
-		echo -e "Password for $x has been changed."
-		##Changes the USER password policy
-		chage -M 90 -m 7 -W 15 $x
-	done
-echo "$LogTime uss: [$UserName]# Passwords have been changed." >> output.log
+    # Extract users with UID >= 1000 (excluding system users and hidden accounts)
+    cut -d: -f1,3 /etc/passwd | awk -F: '$2 >= 1000 {print $1}' > users
 
-	pause
+    # Remove the currently logged-in user and root from the list
+    sed -i "/^$UserName\$/d" users
+    sed -i '/^root$/d' users
+
+    PASS='Cyb3rPatr!0t$'
+    
+    while read -r user; do
+        echo -e "$PASS\n$PASS" | passwd "$user" >> output.log 2>&1
+        echo "Password for $user has been changed."
+        
+        # Update password policy
+        chage -M 90 -m 7 -W 15 "$user"
+    done < users
+
+    echo "$LogTime uss: [$UserName]# Passwords have been changed." >> output.log
+    
+    # Clean up
+    rm -f users
+
+    pause
 }
 
 ##Sets the password policy
